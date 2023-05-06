edc8c9677ae38e29b591f16a01ccad71aa9ab161
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-05-06 09:58:45 +0900

    lint + prettier 導入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = signal('simple-form');
