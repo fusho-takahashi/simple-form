648f88ee2bff9babce8666b4b6bdc98a0fec44da
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-05-06 10:26:16 +0900

    local のapi を叩いてform title を表示する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+type Forms = {
+  title: string;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = signal('simple-form');
+export class AppComponent implements OnInit {
+  title = signal<string>('simple-form');
+  forms = signal<Forms[]>([]);
+  http = inject(HttpClient);
+
+  async ngOnInit(): Promise<void> {
+    const res = await firstValueFrom(this.http.get<{ results: Forms[] }>('http://localhost:8787/api/forms'));
+    this.forms.set(res.results);
+  }
 }
