8424022cdc000449e4b0ce42c09bc6e011257037
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-05-06 09:44:30 +0900

    standalone + signal で動かす

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simple-form';
+  title = signal('simple-form');
 }
